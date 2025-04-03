4aa357598d36f20d244d83198fd65cbb3b04fa11
wekingchen <weking.chen@gmail.com>
2025-04-04 00:35:52 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -22,7 +22,7 @@
 # 修改golang源码以编译xray1.8.8+版本
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
-sed -i '/-linkmode external \\/d' feeds/packages/lang/golang/golang-package.mk
+# sed -i '/-linkmode external \\/d' feeds/packages/lang/golang/golang-package.mk
 
 # 修改frp版本为官网最新v0.61.2 https://github.com/fatedier/frp 格式：https://codeload.github.com/fatedier/frp/tar.gz/v${PKG_VERSION}?
 rm -rf feeds/packages/net/frp
@@ -36,19 +36,3 @@ sed -i 's/PKG_HASH:=ff2a4f04e7732bc77730304e48f97fdd062be2b142ae34c518ab9b9d7a3b
 # 升级zerotier到官方最新版本1.14.2
 sed -i 's/PKG_VERSION:=1.14.1/PKG_VERSION:=1.14.2/' feeds/packages/net/zerotier/Makefile
 sed -i 's/PKG_HASH:=4f9f40b27c5a78389ed3f3216c850921f6298749e5819e9f2edabb2672ce9ca0/PKG_HASH:=c2f64339fccf5148a7af089b896678d655fbfccac52ddce7714314a59d7bddbb/' feeds/packages/net/zerotier/Makefile
-
-# 克隆 coolsnowwolf 的 luci 和 packages 仓库
-# git clone https://github.com/coolsnowwolf/luci.git coolsnowwolf-luci
-# git clone https://github.com/coolsnowwolf/packages.git coolsnowwolf-packages
-
-# 替换luci-app-zerotier
-# rm -rf feeds/luci/applications/luci-app-zerotier
-# cp -r coolsnowwolf-luci/applications/luci-app-zerotier feeds/luci/applications
-
-# 替换zerotier
-# rm -rf feeds/packages/net/zerotier
-# cp -r coolsnowwolf-packages/net/zerotier feeds/packages/net
-
-# 删除克隆的 coolsnowwolf-luci 和 coolsnowwolf-packages 仓库
-# rm -rf coolsnowwolf-luci
-# rm -rf coolsnowwolf-packages
