132598786bb6d1a64eeab14312d55dde3e0cf2f8
wekingchen <weking.chen@gmail.com>
2025-09-23 07:44:29 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -19,6 +19,13 @@
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
 
+# 修改gn为可编译版本
+rm -rf feeds/packages/devel/gn
+wget https://github.com/immortalwrt/packages/archive/a22edf48a23edfcfe212d2dbb83830d69dbb5f2f.zip -O immortalwrtPackages.zip
+unzip immortalwrtPackages.zip
+cp -r packages-a22edf48a23edfcfe212d2dbb83830d69dbb5f2f/devel/gn feeds/packages/devel/
+rm -rf immortalwrtPackages.zip packages-a22edf48a23edfcfe212d2dbb83830d69dbb5f2f
+
 # 修改golang源码以编译xray1.8.8+版本
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 25.x feeds/packages/lang/golang
